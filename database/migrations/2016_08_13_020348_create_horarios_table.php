cfc4a32206344ad8e774b17234842485f593d2a0
VitorBenjamin <vitor.benjamin7@gmail.com>
2016-08-13 23:19:39 -0300

    Criação da tabela de horarios

---
+++ database/migrations/2016_08_13_020348_create_horarios_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHorariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('horarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('horarios');
+    }
+}
