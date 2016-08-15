b3dbda98f0279329226071a07b9e08479346dab1
VitorBenjamin <vitor.benjamin7@gmail.com>
2016-08-15 10:55:12 -0300

    Criação das migrations jogo_time e aposta_jogo

---
+++ database/migrations/2016_08_14_222343_jogo_time.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class JogoTime extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::create('jogo_time', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('jogos_id')->unsigned();
+            $table->integer('times_id')->unsigned();
+            
+        });
+       
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('jogo_time');
+    }
+}
---
+++ database/migrations/2016_08_15_125044_aposta_jogo.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ApostaJogo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+         Schema::create('aposta_jogo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('apostas_id')->unsigned();
+            $table->integer('jogos_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('aposta_jogo');
+    }
+}
