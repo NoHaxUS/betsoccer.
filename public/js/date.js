3e070684aec4fad824f28cc94586ada84cb84b8d
VitorBenjamin <vitor.benjamin7@gmail.com>
2016-11-20 04:41:35 -0300

    Teste corrir bug do horario

--- public/js/date.js
+++ public/js/date.js
@@ -2,7 +2,7 @@ moment().format();
 $(function () {
 	$('#datetimepicker1').datetimepicker({
 		format: 'YYYY-MM-DD H:mm:ss',
-		minDate: moment().startOf('day').hour(23).minute(59)
+		minDate: moment().startOf('day')
 	}
 
 	);
