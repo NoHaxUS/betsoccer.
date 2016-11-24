1809ed8c2a0d0e9d630959e9b5d020216a8fba03
VitorBenjamin <vitor.benjamin7@gmail.com>
2016-11-24 00:05:13 -0300

    Config

--- config/database.php
+++ config/database.php
@@ -47,49 +47,49 @@ return [
     'connections' => [
 
         'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
+        'driver' => 'sqlite',
+        'database' => env('DB_DATABASE', database_path('database.sqlite')),
+        'prefix' => '',
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'host' => 'localhost',
-            'port' => '3306',
-            'database' => 'homestead',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => false,
-            'engine' => null,
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'port' => '3306',
+        'database' => 'homestead',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
+        'strict' => false,
+        'engine' => null,
         ],
 
         'mysql1' => [
-            'driver' => 'mysql',
-            'host' => '209.99.16.14',
-            'port' => '3306',
-            'database' => 'betsocf5_betsoccer00',
-            'username' => 'betsocf5_admin',
-            'password' => 'betsocker123',
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => false,
+        'driver' => 'mysql',
+        'host' => env('DB_HOST', '209.99.16.14'),
+        'port' => env('DB_PORT', '3306'),
+        'database' => env('DB_DATABASE', 'betsocf5_betsoccer00'),
+        'username' => env('DB_USERNAME', 'betsocf5_admin'),
+        'password' => env('DB_PASSWORD', 'betsocker123'),
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
+         'strict' => false,
             'engine' => null,
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
+        'driver' => 'pgsql',
+        'host' => env('DB_HOST', 'localhost'),
+        'port' => env('DB_PORT', '5432'),
+        'database' => env('DB_DATABASE', 'forge'),
+        'username' => env('DB_USERNAME', 'forge'),
+        'password' => env('DB_PASSWORD', ''),
+        'charset' => 'utf8',
+        'prefix' => '',
+        'schema' => 'public',
         ],
 
     ],
@@ -120,15 +120,15 @@ return [
 
     'redis' => [
 
-        'cluster' => false,
+    'cluster' => false,
 
-        'default' => [
-            'host' => env('REDIS_HOST', 'localhost'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
-        ],
+    'default' => [
+    'host' => env('REDIS_HOST', 'localhost'),
+    'password' => env('REDIS_PASSWORD', null),
+    'port' => env('REDIS_PORT', 6379),
+    'database' => 0,
+    ],
 
     ],
 
-];
+    ];
