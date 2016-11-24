05413b710ad9643569a3d195bded0c32d6bd4c58
VitorBenjamin <vitor.benjamin7@gmail.com>
2016-11-23 23:53:44 -0300

    Bug

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |'default' => env('DB_CONNECTION', 'mysql'),
     */
 
-    'default' => 'mysql',
+    'default' => 'mysql1',
 
     /*
     |--------------------------------------------------------------------------
