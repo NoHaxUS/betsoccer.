1785af242f400687fb080817e1fb2b756428038b
Wilder <wildermachadodacruz@gmail.com>
2016-12-06 10:25:08 -0200

    Centralização do botão jogo.maisApostados na jogo.index

--- resources/views/jogo/index.blade.php
+++ resources/views/jogo/index.blade.php
@@ -7,7 +7,7 @@
         {{ csrf_field() }}
         <div >
             <h3 align="center">{{date('d/m/Y',strtotime($d))}}</h3>
-            <a class="btn btn-success" href="{{ route('jogo.maisApostados') }}">Jogos com mais apostas</a>
+            <p class="text-center"><a class="btn btn-success" href="{{ route('jogo.maisApostados') }}">Jogos com mais apostas</a></p>
         </div>
         @foreach(campsHora($d,$jogos) as $camp)
         <h5>{{$camp}}</h5>
--- resources/views/jogo/maisapostados.blade.php
+++ resources/views/jogo/maisapostados.blade.php
@@ -9,30 +9,30 @@
             </div>
             {{ csrf_field() }}
             @foreach($jogos as $indice => $jogo)
-            <h4>{{$jogo->time[0]->descricao_time}} x {{$jogo->time[1]->descricao_time}}</h4>
+                <h4>{{$jogo->time[0]->descricao_time}} x {{$jogo->time[1]->descricao_time}}</h4>
                 <h4>{{'Total de apostas: '.$jogo->apostas()->count()}} </h4>
-            <div class="table-responsive">
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Palpite</th>
-                        <th>Quantidade de apostas</th>
-                        <th>Valor total</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-
-                    @foreach($palpites[$indice] as $key => $palpite)
+                <div class="table-responsive">
+                    <table>
+                        <thead>
                         <tr>
-                            <td>{{$key}}</td>
-                            <td>{{$palpite['qtd_'.$key]}}</td>
-                            <td>{{number_format($palpite['total_'.$key], 2, ',', '.')}}</td>
+                            <th>Palpite</th>
+                            <th>Quantidade de apostas</th>
+                            <th>Valor total</th>
                         </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-                @endforeach
+                        </thead>
+                        <tbody>
+
+                        @foreach($palpites[$indice] as $key => $palpite)
+                            <tr>
+                                <td>{{$key}}</td>
+                                <td>{{$palpite['qtd_'.$key]}}</td>
+                                <td>{{number_format($palpite['total_'.$key], 2, ',', '.')}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
