a1b0658860de2f5a294b27e96cfa42c1c69872a1
Wilder <wildermachadodacruz@gmail.com>
2017-01-06 12:33:22 -0200

    Alteração de método ultima em ApostaController, restringido para apostas após acerto

--- app/Http/Controllers/ApostaController.php
+++ app/Http/Controllers/ApostaController.php
@@ -523,7 +523,7 @@ class ApostaController extends Controller
         if (!is_null($resposta)):                                       //Se retornou restrição
             return response()->json($resposta, $resposta['erro']);      //Retorna json com restrição encontrada
         endif;
-        $aposta = $user->apostas->first();                               //Busca última aposta feita pelo usuário
+        $aposta = Aposta::recentes($user)->first();                     //Busca última aposta feita pelo usuário
         if (is_null($aposta)):                                          //Se aposta for nula
             return response()->json(['aposta' => 'inexistente'], 403);  //Retorna json informando
         endif;
