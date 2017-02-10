1498bc8e9ca8fdb3dca761d00a450a69961f8c46
Wilder <wildermachadodacruz@gmail.com>
2017-02-09 23:49:21 -0200

    Correção migration de acertos

--- database/migrations/2017_02_09_090408_create_acertos_table.php
+++ database/migrations/2017_02_09_090408_create_acertos_table.php
@@ -13,17 +13,18 @@ class CreateAcertosTable extends Migration
     public function up()
     {
         Schema::create('acertos', function (Blueprint $table) {
+            $table->engine = 'MyISAM';
             $table->increments('id');
             $table->integer('cambista_id')->unsigned();
             $table->integer('gerente_id')->unsigned();
             $table->integer('qtd_apostas')->unsigned();
             $table->integer('qtd_jogos')->unsigned();
-            $table->double('comissao_simples',15,2);
-            $table->double('comissao_mediana',15,2);
-            $table->double('comissao_maxima',15,2);
-            $table->double('total_apostado',15,2);
-            $table->double('total_premiacao',15,2);
-            $table->double('liquido',15,2);
+            $table->double('comissao_simples', 15, 2);
+            $table->double('comissao_mediana', 15, 2);
+            $table->double('comissao_maxima', 15, 2);
+            $table->double('total_apostado', 15, 2);
+            $table->double('total_premiacao', 15, 2);
+            $table->double('liquido', 15, 2);
             $table->timestamps();
             $table->foreign('cambista_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('gerente_id')->references('id')->on('users')->onDelete('cascade');
